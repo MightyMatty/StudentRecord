2d67a6c58b7990ed6be8a393afc5c89fcbc717ea
Matthew Song <jc0319@outlook.com>
2017-11-20 09:56:09 +0000

    Class 20/11/17

--- StudentObject/StudentObject/Program.cs
+++ StudentObject/StudentObject/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.IO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,65 @@ namespace StudentObject
 {
     class Program
     {
+        static String[] Records = new string[5];
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Students Record!");
+            while (true)
+            {
+                Console.WriteLine("Select Option:");
+                Console.WriteLine("1.Read the File");
+                Console.WriteLine("2.Find a Record");
+
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                if (userInput == 1)
+                {
+                    Console.WriteLine("Please Enter The File Name");
+                    ReadFile();
+                }
+                if (userInput == 2)
+                {
+                    Console.WriteLine("Please enter the index number");
+                    Tab();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+            }
+        }
+
+        private static void Tab()
+        {
+
+
+            int index = Convert.ToInt32(Console.ReadLine());
+            var read = Records[index].Replace(",", "\t");
+            Console.WriteLine(read);
+
+        }
+
+        private static void ReadFile()
+        {
+            string Filename = Console.ReadLine();
+            using (StreamReader reader = new StreamReader(Filename))
+            {
+                int i = 0;
+                while (!reader.EndOfStream)
+                {
+                    Records[i] = reader.ReadLine();
+                    i++;
+                }
+
+                foreach (var Record in Records)
+                {
+                    Console.WriteLine(Record);
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
+
+ 
---
+++ StudentObject/StudentObject/Student.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentObject
+{
+    public class Student
+    {
+        public Student(int number, string first, string last, DateTime dob,String grade)
+        {
+            Number = number;
+            FirstName = first;
+            LastName = last;
+            DateOfBirth = dob;
+            Grade = grade;
+        }
+        public int Number { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get;private set; }
+        public string Grade { get; set; }
+
+
+
+    }
+}
